8ace6cb4d0a175499da74a92b5905f68dbd75f71
Daniel Furman <djfurman@gmail.com>
2019-07-09 19:46:15 -0400

    Add locust file for basic API hits.

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,21 @@
 from locust import HttpLocust, TaskSet, task
 
+
 class UserBehavior(TaskSet):
-    pass
+    @task(1)
+    def fetch_api_root(self):
+        self.client.get("/api")
+
+    @task(2)
+    def fetch_api_v1(self):
+        self.client.get("/api/v1")
+
+    @task(5)
+    def fetch_api_v1_articles(self):
+        self.client.get("/api/v1/articles")
+
 
 class ApiUser(HttpLocust):
     task_set = UserBehavior
-    min_wait = 30
-    max_wait = 1000
+    min_wait = 30  # thirty milliseconds
+    max_wait = 1000  # one second
